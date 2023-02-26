c13535b5ce85c9ca99b737f10eabbdadf21d160e
Ahmet Balaman <56163828+ahmetbalaman@users.noreply.github.com>
2023-02-26 15:07:45 +0300

    v1

---
+++ lib/starting_services/future_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FutureServices extends StatefulWidget {
+  const FutureServices({super.key});
+
+  @override
+  State<FutureServices> createState() => _FutureServicesState();
+}
+
+class _FutureServicesState extends State<FutureServices> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
